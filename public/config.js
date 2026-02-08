b87ca1643e9b93482d389929b471719b4344f36f
Sten <mail@stenterstal.com>
2026-02-08 17:08:28 +0100

    Environment variables in SPA

---
+++ public/config.js
@@ -0,0 +1,3 @@
+window._env_ = {
+  API_BASE_URL: "http://10.10.20.50:8000",
+};
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,8 @@
-export const API_BASE_URL = "http://10.10.20.50:8000";
+interface Window {
+  _env_?: {
+    API_BASE_URL?: string;
+    [key: string]: any;
+  };
+}
+
+export const API_BASE_URL = (window as Window)._env_?.API_BASE_URL|| "http://10.10.20.50:8000";
