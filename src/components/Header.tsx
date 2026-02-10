8bded7d1085f93dde12cb0418965327895dcf546
Sten <mail@stenterstal.com>
2026-02-10 23:05:31 +0100

    Switched old import to new one

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <NavLink to="/" className={"flex flex-1"}>
           <img src={logo} className="block m-0 p-0 h-15 md:h-20 w-auto"/> 
         </NavLink>
-        {auth.token &&
+        {auth.isAuthenticated &&
         <div className={"p-4 md:p-6 min-w content-center cursor-pointer"} onClick={() => auth.logout()}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
