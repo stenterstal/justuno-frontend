45b39c31bbb6797bc34a7783f527e96486002e0f
Sten <mail@stenterstal.com>
2026-02-13 13:34:39 +0100

    Remove uno logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router";
-import logo from '../assets/logo.svg'
 
 export default function Header() {
   return (
     <header className="w-full h-20 md:h-25 p-2 md:p bg-slate-100 flex flex-1">
       <nav className="flex flex-1 relative max-w-4xl mx-auto items-center">
         <NavLink to="/" className={"flex flex-1"}>
-          <img src={logo} className="block m-0 p-0 h-15 md:h-20 w-auto"/> 
+          {/* <img src={logo} className="block m-0 ml-4 md:ml-0 p-0 h-8 md:h-12 w-auto"/> */}
+          <h1 className="block m-0 ml-4 md:ml-0 p-0 font-bold text-2xl">JUSTUNO</h1>
         </NavLink>
       </nav>
     </header>
