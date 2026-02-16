d9825f32698f61506e0f25fa78b9b5b07c8be6e2
Sten <mail@stenterstal.com>
2026-02-16 17:40:57 +0100

    Added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router";
+import Logo from "../assets/logo.svg?react";
 
 export default function Header() {
   return (
-    <header className="w-full h-20 md:h-25 p-2 md:p bg-slate-100 flex flex-1">
-      <nav className="flex flex-1 relative max-w-4xl mx-auto items-center">
-        <NavLink to="/" className={"flex flex-1"}>
-          {/* <img src={logo} className="block m-0 ml-4 md:ml-0 p-0 h-8 md:h-12 w-auto"/> */}
-          <h1 className="block m-0 ml-4 md:ml-0 p-0 font-bold text-2xl">JUSTUNO</h1>
+    <header className="w-full h-20 md:h-25 bg-slate-100">
+      <nav className="max-w-4xl mx-auto h-full flex items-center pl-8">
+        <NavLink to="/" className="h-full flex items-center">
+          <Logo className="h-full w-auto object-contain" />
         </NavLink>
       </nav>
     </header>
